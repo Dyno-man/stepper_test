f24d94bff389f8790651b852f9e0b7a63d226fb0
Dino <130256998+Dyno-man@users.noreply.github.com>
2024-07-31 19:42:54 -0400

    Update main.rs
    Added threads to run motors asynchronously

--- stepper_lib_tester/src/main.rs
+++ stepper_lib_tester/src/main.rs
@@ -5,8 +5,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut stepper1 = stepper_lib::stepper_rotate::Stepper::new(17,27,22,23, 2048,Duration::from_millis(2), true)?;
     let mut stepper2 = stepper_lib::stepper_rotate::Stepper::new(5, 6, 13, 12, 4096, Duration::from_millis(2), true)?; 
     
-    stepper1.rotate()?;
-    stepper2.rotate()?;
+    let handle_step1 = thread::spawn(move || {
+        stepper1.rotate()?.unwrap();
+    });
+
+    let handle_step2 = thread::spawn(move || {
+        stepper2.rotate()?.unwrap();
+    });
+
+    handle_step1.join().unwrap();
+    handle_step2.join().unwrap();
 
     Ok(())
 }
