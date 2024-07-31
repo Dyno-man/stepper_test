3b208eed6392c94528a05a90e2e32009d8cf2336
Dino <130256998+Dyno-man@users.noreply.github.com>
2024-07-31 19:33:31 -0400

    Update main.rs
    Added a second stepper motor to the raspberry pi

--- stepper_lib_tester/src/main.rs
+++ stepper_lib_tester/src/main.rs
@@ -1,11 +1,13 @@
-use stepper_lib::stepper_rotate;
 use std::error::Error;
 
 use std::time::Duration;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut stepper1 = stepper_lib::stepper_rotate::Stepper::new(17,27,22,23, 4096,Duration::from_millis(2), true)?;
+    let mut stepper1 = stepper_lib::stepper_rotate::Stepper::new(17,27,22,23, 2048,Duration::from_millis(2), true)?;
+    let mut stepper2 = stepper_lib::stepper_rotate::Stepper::new(5, 6, 13, 12, 4096, Duration::from_millis(2), true)?; 
+    
     stepper1.rotate()?;
+    stepper2.rotate()?;
 
     Ok(())
 }
