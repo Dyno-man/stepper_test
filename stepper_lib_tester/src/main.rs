d8114e27a2fdc6f45cc05165b2efc53390eb29cc
Dino <130256998+Dyno-man@users.noreply.github.com>
2024-07-31 19:43:54 -0400

    Update main.rs
    Cannot use the ? operator inside the thread enclosure

--- stepper_lib_tester/src/main.rs
+++ stepper_lib_tester/src/main.rs
@@ -6,11 +6,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut stepper2 = stepper_lib::stepper_rotate::Stepper::new(5, 6, 13, 12, 4096, Duration::from_millis(2), true)?; 
     
     let handle_step1 = thread::spawn(move || {
-        stepper1.rotate()?.unwrap();
+        stepper1.rotate().unwrap();
     });
 
     let handle_step2 = thread::spawn(move || {
-        stepper2.rotate()?.unwrap();
+        stepper2.rotate().unwrap();
     });
 
     handle_step1.join().unwrap();
